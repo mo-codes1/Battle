056ddbcaef78839f467e20d192dd21c6055a606d
dean <d.lewis.rave@gmail.com>
2021-11-04 17:02:55 +0000

    Astarted implementing hit points

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -4,3 +4,10 @@ feature "Player 1 has attacked player 2" do
     expect(page).to have_content 'Jacob attacked Mo'
     end
 end
+
+feature "Player 2 hp has decreased by 10" do
+  scenario "Player 2 has lost health" do
+    sign_in_and_play
+    expect(page).to have_content 'Mo HP = 90'
+  end
+end 
