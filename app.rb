a64564784086019bb8862be369773d14c7fab4e0
Mohammed Salah <mohammed_salah@icloud.com>
2021-11-09 08:24:31 +0000

    Implemented hit points

--- app.rb
+++ app.rb
@@ -25,8 +25,11 @@ class Battle < Sinatra::Base
     end
 
     get '/attack' do
-     @player_1_name = session[:player_1_name]
-     @player_2_name = session[:player_2_name]
+     @player_1 = $player_1
+     @player_2 = $player_2
+
+     @player_1.attack(@player_2)
+     
      erb :attack
    end
   end
--- lib/play.rb
+++ lib/play.rb
@@ -1,6 +1,16 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
+
   def initialize(name)
       @name = name
+      @hp = 100
+  end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hp -= 10
   end
 end 
